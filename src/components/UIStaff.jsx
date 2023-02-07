4a9520dd5ced2af34c1409d7f063b9b408daa78c
Kuper <tomykup@gmail.com>
2023-02-07 17:04:34 -0300

    fixed spaces

--- src/components/UIStaff.jsx
+++ src/components/UIStaff.jsx
@@ -13,36 +13,28 @@ export function UIStaff() {
           name="Vanina Minervini"
           description="Lorem ipsum dolor sit amet consectetur adipiscing elit a, nec curae
             molestie imperdiet nisl rhoncus turpis, sed tristique suspendisse
-            massa felis in iaculis. Aliquam viverra nisl sapien accumsan etiam
-            at, enim montes lacinia convallis pellentesque nam augue, dignissim
-            ullamcorper vivamus dictumst mus."
+            massa felis in iaculis."
         />
         <UIStaffCard
           image="https://m9p8e5u6.rocketcdn.me/wp-content/uploads/2019/04/shutterstock_Nestor-Rizhniak.jpg"
           name="Oscar Ocampos"
           description="Lorem ipsum dolor sit amet consectetur adipiscing elit a, nec curae
             molestie imperdiet nisl rhoncus turpis, sed tristique suspendisse
-            massa felis in iaculis. Aliquam viverra nisl sapien accumsan etiam
-            at, enim montes lacinia convallis pellentesque nam augue, dignissim
-            ullamcorper vivamus dictumst mus."
+            massa felis in iaculis."
         />
         <UIStaffCard
           image="https://www.cmolaflorida.com/images/equipo/med_width_1659346699ariadna-dentista.jpg"
           name="Camila Jiorjieva"
           description="Lorem ipsum dolor sit amet consectetur adipiscing elit a, nec curae
             molestie imperdiet nisl rhoncus turpis, sed tristique suspendisse
-            massa felis in iaculis. Aliquam viverra nisl sapien accumsan etiam
-            at, enim montes lacinia convallis pellentesque nam augue, dignissim
-            ullamcorper vivamus dictumst mus."
+            massa felis in iaculis."
         />
         <UIStaffCard
           image="https://yt3.googleusercontent.com/amA2qRpg3ItU9h04goS5486Iz-l89h5fxBMqOZXO3kAhikcvLyuvWiJstD5U0SOYI2wZEiAOzg=s900-c-k-c0x00ffffff-no-rj"
           name="Odonotologa extra"
           description="Lorem ipsum dolor sit amet consectetur adipiscing elit a, nec curae
             molestie imperdiet nisl rhoncus turpis, sed tristique suspendisse
-            massa felis in iaculis. Aliquam viverra nisl sapien accumsan etiam
-            at, enim montes lacinia convallis pellentesque nam augue, dignissim
-            ullamcorper vivamus dictumst mus."
+            massa felis in iaculis."
         />
       </div>
     </div>
